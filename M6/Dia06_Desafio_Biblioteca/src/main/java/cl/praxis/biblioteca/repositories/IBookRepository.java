654bc3875405ce560fd2d703e2ec7ffedad81074
Francisco Carmona <flcarmonac@yahoo.com>
2024-07-22 19:29:42 -0400

    Dia06 Desafio Biblioteca

--- M6/Dia06_Desafio_Biblioteca/src/main/java/cl/praxis/biblioteca/repositories/IBookRepository.java
+++ M6/Dia06_Desafio_Biblioteca/src/main/java/cl/praxis/biblioteca/repositories/IBookRepository.java
@@ -2,6 +2,8 @@ package cl.praxis.biblioteca.repositories;
 
 import cl.praxis.biblioteca.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IBookRepository extends JpaRepository<Book, Integer> {
 }
