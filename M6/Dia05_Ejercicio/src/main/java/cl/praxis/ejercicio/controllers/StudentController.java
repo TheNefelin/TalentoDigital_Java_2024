7f6cd9dfff5714dcc505bfb723715301cc840095
Francisco Carmona <flcarmonac@yahoo.com>
2024-07-23 18:43:27 -0400

    Dia05 Ejercicio

--- M6/Dia05_Ejercicio/src/main/java/cl/praxis/ejercicio/controllers/StudentController.java
+++ M6/Dia05_Ejercicio/src/main/java/cl/praxis/ejercicio/controllers/StudentController.java
@@ -7,49 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
     @Autowired
     private IBaseCRUD<Student> service;
 
-    @GetMapping
+    @GetMapping("/getAll")
     public String getAll(Model model) {
         model.addAttribute("estudiantes", service.getAll());
         return "students";
     }
 
-    @GetMapping("/create")
-    public String showCreateForm(Model model) {
-        model.addAttribute("student", new Student());
-        return "student-form";
-    }
-
-    @PostMapping("/create")
-    public String createStudent(@ModelAttribute Student student) {
-        service.add(student);
-        return "redirect:/student";
-    }
-
-    @GetMapping("/update/{id}")
-    public String showUpdateForm(@PathVariable int id, Model model) {
-        Student student = service.getById(id);
-        if (student != null) {
-            model.addAttribute("student", student);
-            return "student-form";
-        }
-        return "redirect:/student";
-    }
-
-    @PostMapping("/update/{id}")
-    public String updateStudent(@PathVariable int id, @ModelAttribute Student student) {
-        service.update(id, student);
-        return "redirect:/student";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteStudent(@PathVariable int id) {
-        service.delete(id);
-        return "redirect:/student";
+    @GetMapping("/{id}")
+    public String getById(Model model) {
+        model.addAttribute("estudiantes", service.getAll());
+        return "students";
     }
 }
