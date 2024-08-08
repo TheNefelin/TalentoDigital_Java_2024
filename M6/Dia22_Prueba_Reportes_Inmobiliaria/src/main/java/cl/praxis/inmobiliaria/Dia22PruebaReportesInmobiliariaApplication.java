85aee2f679f76e30f83a7ea7a58450c3af65b3bf
Francisco Carmona <flcarmonac@yahoo.com>
2024-08-08 17:11:45 -0400

    Dia22 Prueba Reportes Inmobiliaria

---
+++ M6/Dia22_Prueba_Reportes_Inmobiliaria/src/main/java/cl/praxis/inmobiliaria/Dia22PruebaReportesInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package cl.praxis.inmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dia22PruebaReportesInmobiliariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dia22PruebaReportesInmobiliariaApplication.class, args);
+    }
+
+}
---
+++ M6/Dia22_Prueba_Reportes_Inmobiliaria/src/test/java/cl/praxis/inmobiliaria/Dia22PruebaReportesInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.praxis.inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dia22PruebaReportesInmobiliariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
