23fc6fb2dfcd409591d7d64625a66d8e648b5c9d
Francisco Carmona <flcarmonac@yahoo.com>
2024-08-14 22:56:23 -0400

    Pruebas

---
+++ Pruebas/VeterinariaTD/src/main/java/cl/praxis/veterinaria/controllers/Controller.java
@@ -0,0 +1,4 @@
+package cl.praxis.veterinaria.controllers;
+
+public class Controller {
+}
---
+++ Pruebas/VeterinariaTD/src/test/java/cl/praxis/veterinaria/VeterinariaTdApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.praxis.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaTdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
