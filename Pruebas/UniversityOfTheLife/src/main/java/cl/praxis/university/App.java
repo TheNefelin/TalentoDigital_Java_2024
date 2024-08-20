0c1a4897f3bcd42083846d425bc5ee3b8b0c1997
Francisco Carmona <flcarmonac@yahoo.com>
2024-08-20 17:36:16 -0400

    Prueba UniversityOfTheLife

--- Pruebas/UniversityOfTheLife/src/main/java/cl/praxis/university/App.java
+++ Pruebas/UniversityOfTheLife/src/main/java/cl/praxis/university/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversityOfTheLifeApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(UniversityOfTheLifeApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
